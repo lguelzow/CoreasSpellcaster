5f583df2e510ec8cb21b016fe18db22849c561db
jelenakhlr <108803123+jelenakhlr@users.noreply.github.com>
2023-08-16 09:54:11 +0200

    use ranges for generating runnumbers

--- utils/runNumberGenerator.py
+++ utils/runNumberGenerator.py
@@ -44,30 +44,30 @@ class runNumberGenerator:
     def __init__(self):
 
         self.zenithDict = {
-                             65.0 : 0,
-                             67.5 : 1,
-                             70.0 : 2,
-                             72.5 : 3,
-                             75.0 : 4,
-                             77.5 : 5,
-                             80.0 : 6,
-                             82.5 : 7,
-                             85.0 : 8,
-                                    }
+                             (0,65.0) : 0,
+                             (65.1,67.5) : 1,
+                             (67.6,70.0) : 2,
+                             (70.1, 72.5) : 3,
+                             (72.6,75.0) : 4,
+                             (75.1,77.5) : 5,
+                             (77.6, 80.0) : 6,
+                             (80.1,82.5) : 7,
+                             (82.6,85.0) : 8,
+                             (85.1,90) : 9,
+                            }
         
-        # TODO: fix this mess
+        
         self.azimuthDict = {
-                             0.0  : 0,
-                            #  45.0 : 1,
-                             38.0 : 1,
-                             90.0 : 2,
-                             135.0: 3,
-                             180.0: 4,
-                             225.0: 5,
-                             270.0: 6,
-                             -270.0: 6,
-                             315.0: 7,
-                             360.0: 8,
+                             (0.0, 44.9)  : 0,
+                             (45, 89.9) : 1,
+                             (90.0, 134.9) : 2,
+                             (135.0, 179.9): 3,
+                             (180.0, 224.9): 4,
+                             (225.0, 269.9): 5,
+                             (270.0, 314.9): 6,
+                             (315.0, 360): 7,
+                             (-0.1, -180) : 8,
+                             (-180.1, -360) : 9,
                                     }
         
         # TODO: update for more particles
@@ -79,12 +79,19 @@ class runNumberGenerator:
 
 
     def getZenithID(self, zenith_angle):
-        return self.zenithDict[zenith_angle]
+        for (lower, upper), category in self.zenithDict.items():
+            if lower <= zenith_angle <= upper:
+                return category
+        raise ValueError("Zenith angle not found in any runNumber category")
+
     
 
     
     def getAzimuthID(self, azimuth_angle):
-        return self.azimuthDict[azimuth_angle]
+        for (lower, upper), category in self.azimuthDict.items():
+            if lower <= azimuth_angle <= upper:
+                return category
+        raise ValueError("Azimuth angle not found in any runNumber category")
     
 
 
